5c805760d63ab14cb809feb2bdbd2aeb6c67d7ad
Tim4220 <mcheung.yyz@gmail.com>
2024-11-23 20:09:20 -0500

    Changed main

--- main.py
+++ main.py
@@ -8,20 +8,36 @@ while True:
         if event.type == pygame.quit():
             raise SystemExit'''
 import pygame
-import random
 import sys
+import os
 
 pygame.init()
 
 WIDTH, HEIGHT = 1200, 700
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-BLUE = (0, 0, 255)
-GREEN = (0, 255, 0)
+WHITE = (254, 254, 254)
+BLACK = (25, 25, 25)
+RED = (255, 25, 25)
+BLACK = (23, 23, 23)
+GREEN = (25, 255, 25)
+ALPHA = (0, 255, 0)
+
+FPS = 30
+
+class employee(pygame.sprite.Sprite):
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.images = []
+        for i in range(1,7):
+            img = pygame.image.load(os.path.join('images', 'hero' + str(i) + '.png')).convert()
+            img.convert_alpha()  # optimise alpha
+            img.set_colorkey(ALPHA)  # set alpha
+            self.images.append(img)
+            self.image = self.images[0]
+            self.rect = self.image.get_rect()
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Self-Destruct Challenge")
+pygame.display.set_caption("PLACEHOLDER")
 clock = pygame.time.Clock()
 font = pygame.font.Font(None, 36)
 
@@ -29,12 +45,6 @@ player_size = 50
 player_x, player_y = WIDTH // 2, HEIGHT // 2
 player_speed = 7
 
-spike_width, spike_height = 100, 20
-spike_x, spike_y = random.randint(0, WIDTH - spike_width), random.randint(0, HEIGHT - spike_height)
-
-button_width, button_height = 60, 30
-button_x, button_y = random.randint(0, WIDTH - button_width), random.randint(0, HEIGHT - button_height)
-button_pressed = False
 
 timer = 60 * 30
 game_over = False
@@ -56,25 +66,9 @@ while running:
     if keys[pygame.K_DOWN] and player_y < HEIGHT - player_size:
         player_y += player_speed
 
-    if (spike_x < player_x < spike_x + spike_width or spike_x < player_x + player_size < spike_x + spike_width) and \
-       (spike_y < player_y < spike_y + spike_height or spike_y < player_y + player_size < spike_y + spike_height):
-        game_over = True
-
-    if (button_x < player_x < button_x + button_width or button_x < player_x + player_size < button_x + button_width) and \
-       (button_y < player_y < button_y + button_height or button_y < player_y + player_size < button_y + button_height):
-        button_pressed = True
-
-    if button_pressed:
-        spike_x, spike_y = random.randint(0, WIDTH - spike_width), random.randint(0, HEIGHT - spike_height)
-        button_pressed = False
-
     timer -= 1
     if timer <= 0:
         running = False
-
-    pygame.draw.rect(screen, BLACK, (player_x, player_y, player_size, player_size))
-    pygame.draw.rect(screen, RED, (spike_x, spike_y, spike_width, spike_height))
-    pygame.draw.rect(screen, GREEN, (button_x, button_y, button_width, button_height))
 
     timer_text = font.render(f"Time Left: {timer // 30}s", True, BLACK)
     screen.blit(timer_text, (10, 10))
